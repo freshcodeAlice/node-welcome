2d18af2f5dde3475895bc8320a1af8af02c67fe0
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-05 18:42:12 +0300

    Home page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+
 const PORT = 3000
 
 let count = 0
 
 const requestListener = (req, res) => {
-  res.end(`hello from node! your request is ${count++}`)
+  fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+    res.end(data)
+  })
+  //   res.end(`hello from node! your request is ${count++}`)
 }
 
 const server = http.createServer(requestListener)
