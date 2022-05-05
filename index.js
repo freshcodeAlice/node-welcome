5396f68a0a87189f7ec47bceef5e5c8bc10ce32e
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-05 19:40:51 +0300

    mini-SSR app

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const http = require('http')
-const fs = require('fs')
+const fs = require('fs').promises
 
 const PORT = 3000
 
 let count = 0
+const users = []
 
-const requestListener = (req, res) => {
-  fs.readFile('./views/index.html', 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err)
+const requestListener = async (req, res) => {
+  const { url, method } = req
+
+  if (method === 'GET') {
+    if (url === '/') {
+      const data = await fs.readFile('./views/index.html', 'utf-8')
+      return res.end(data)
     }
-    res.end(data)
-  })
-  //   res.end(`hello from node! your request is ${count++}`)
+
+    if (url === '/about.html') {
+      const data = await fs.readFile('./views/about.html', 'utf-8')
+      return res.end(data)
+    }
+    if (url === '/contacts.html') {
+      const data = await fs.readFile('./views/contacts.html', 'utf-8')
+      return res.end(data)
+    }
+  }
+
+  if (method === 'POST') {
+    if (url === '/create-user') {
+      let jsonString = ''
+      req.on('data', chunk => {
+        jsonString += chunk
+      })
+      req.on('end', () => {
+        const user = JSON.parse(jsonString)
+        delete user.password
+        user.id = users.length
+        users.push(user)
+        console.log(users)
+        res.end(JSON.stringify(user))
+      })
+    }
+  }
+
+  const data = await fs.readFile('./views/404.html', 'utf-8')
+  return res.end(data)
 }
 
 const server = http.createServer(requestListener)
