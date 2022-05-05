acfcb0ca0d148a135915f37edd216f20aa6ad1aa
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-05 17:30:49 +0300

    how require works

---
+++ index.js
@@ -0,0 +1,25 @@
+const data = require('./utils/math.js')
+
+require('kfuedfikuywer')
+/* How require works:
+
+Resolving -> Loading -> Wrapping -> Evaluation -> Caching
+
+I
+    core_modules
+    node_modules
+II
+    (Project)
+   2.1 .js
+        .json
+    2.2 directory
+    2.2.1 package.json -> "main": './entrypoint.js'
+    2.2.2 index.js | .json
+III 
+throw new Error
+
+
+
+
+
+*/
